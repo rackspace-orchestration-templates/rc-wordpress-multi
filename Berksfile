6570178b7a6e7ce42e9482e8d99a7914494340ae
Chris Hultin <chris.hultin@rackspace.com>
2015-10-12 13:21:25 -0500

    Initial Commit

---
+++ Berksfile
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# ^syntax detection
+
+source "http://api.berkshelf.com"
+
+cookbook 'apt'
+cookbook 'apache2',
+  :git => 'https://github.com/opscode-cookbooks/apache2.git'
+cookbook 'build-essential'
+cookbook 'firewall'
+cookbook 'memcached'
+cookbook 'mysql',
+  :git => 'https://github.com/opscode-cookbooks/mysql'
+cookbook 'wordpress',
+  :git => 'https://github.com/opscode-cookbooks/wordpress'
+cookbook 'php',
+  :git => 'https://github.com/opscode-cookbooks/php'
+cookbook 'sysctl',
+  :git => 'https://github.com/spheromak/sysctl-cookbook',
+  :ref => '796678a25e1c83f7f5810c1b507acaedd48403c9'
+cookbook 'vsftpd',
+  :git => 'https://github.com/optiflows-cookbooks/vsftpd',
+  :ref => '813744f1d184bdb79c68737a12ed5221825b925c'
+cookbook 'monit',
+  :git => 'https://github.com/apsoto/monit'
+cookbook 'hollandbackup',
+  :git => 'https://github.com/brint/hollandbackup'
+cookbook 'varnish'
+cookbook 'lsyncd',
+  :git => 'https://github.com/dgivens/chef-lsyncd/'
+cookbook 'rax-wordpress',
+  :git => 'https://github.com/brint/rax-wordpress-cookbook'
+cookbook 'cron',
+  :git => 'https://github.com/opscode-cookbooks/cron',
+  :ref => '8007fc9ef6083e04554d729b4ffaef4e7ca81d7a'
+cookbook 'rax-mysql-tunables',
+  :git => 'https://github.com/JasonBoyles/rax-mysql-tunables.git',
+  :ref => 'b5d2179471fb727a0c78b0d2647cc78851475dff'
+cookbook 'rax-firewall',
+  :git => 'https://github.com/brint/rax-firewall-cookbook'
---
+++ test/fabric/web_master.py
@@ -0,0 +1,73 @@
+import os
+from fabric.api import env, task
+from envassert import detect, file, package, port, process, service, user
+from hot.utils.test import get_artifacts
+
+
+@task
+def apache():
+    env.platform_family = detect.detect()
+
+    apache_config = "/etc/apache2/sites-enabled/wordpress.conf"
+    web_user = "www-data"
+    www_dir = "/var/www/vhosts/example.com"
+
+    assert package.installed("apache2")
+    assert process.is_up("apache2")
+    assert service.is_enabled("apache2")
+    assert port.is_listening(8080)
+    assert file.has_line(apache_config, "VirtualHost *:8080")
+    assert file.owner_is(www_dir, web_user)
+
+
+@task
+def lsyncd():
+    env.platform_family = detect.detect()
+
+    wordpress_user = "wp_user"
+    web_group = "www-data"
+    ssh_dir = "/var/www/vhosts/example.com/.ssh"
+    private_key = os.path.join(ssh_dir, "id_rsa")
+
+    assert package.installed("lsyncd"), 'lsyncd is not installed'
+    assert process.is_up("lsyncd"), 'lsyncd is not up'
+    assert service.is_enabled("lsyncd"), 'lsyncd is not enabled'
+    assert user.exists(wordpress_user), 'wp_user user does not exist'
+    assert user.exists('wp_user').get('passwd') != '!', 'wp_user pass missing'
+    assert user.is_belonging_group(wordpress_user, web_group), 'wp_user does belong to correct group'
+    assert file.dir_exists(ssh_dir), 'ssh directory does not exist'
+    assert file.owner_is(ssh_dir, wordpress_user), 'wp_user does not own ssh directory'
+    assert file.owner_is(private_key, wordpress_user), 'wp_user does not own ssh key'
+    assert file.mode_is(private_key, 600), 'ssh key is not set to correct mode'
+
+
+@task
+def varnish():
+    env.platform_family = detect.detect()
+
+    varnish_config = "/etc/varnish/default.vcl"
+
+    assert package.installed("varnish")
+    assert process.is_up("varnishd")
+    assert service.is_enabled("varnish")
+    assert port.is_listening(80)
+    assert file.has_line(varnish_config, "backend master")
+    assert file.has_line(varnish_config, "backend local")
+
+
+@task
+def monitoring():
+    env.platform_family = detect.detect()
+
+    monitor_config = "/etc/rackspace-monitoring-agent.cfg"
+
+    assert package.installed("rackspace-monitoring-agent")
+    assert process.is_up("rackspace-monitoring-agent")
+    assert service.is_enabled("rackspace-monitoring-agent")
+    assert file.exists(monitoring_config)
+
+
+@task
+def artifacts():
+    env.platform_family = detect.detect()
+    get_artifacts()
